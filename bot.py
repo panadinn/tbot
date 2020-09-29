abeb4a06302208471cfe7c80cb0078902dd3a7b5
panadinn <panadinn@gmail.com>
2020-09-29 14:17:11 +0300

    Научил анекдотам

--- bot.py
+++ bot.py
@@ -1,12 +1,23 @@
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from settings import TG_TOKEN, TG_API_URL
+from bs4 import BeautifulSoup
+import requests
 
 # функция sms() будет вызвана пользователем при отправке команды  start
 def sms(bot, update):
     print('Кто-то отправил команду /start. Что мне делать?') # вывод при отправке команды /start
+    my_keyboard = ReplyKeyboardMarkup([['Анекдот'], ['Начать']], resize_keyboard=True) #добавляем кнопку
     bot.message.reply_text('Здравствуйте, {}!  \n'
-                           'Поговорите со мной!'.format(bot.message.chat.first_name)) # отправим ответ
+                           'Поговорите со мной!'.format(bot.message.chat.first_name), reply_markup=my_keyboard) # отправим ответ
 
+def get_anecdote(bot, update):
+    receive = requests.get('http://anekdotme.ru/random') #отправляем запрос к странице
+    page = BeautifulSoup(receive.text, "html.parser") #подключаем html парсер, подключаем текст страницы
+    find = page.select('.anekdot_text') # из страницы html получаем  class="anekdot_text"
+    for text in find:
+        page = (text.getText().strip()) # из class="anekdot_text"  получаем текст и убираем пробелы по сторонам
+    bot.message.reply_text(page) # отправлячем  один анекдот, последний
 
 # функция parrot() отвечает тем же сообщением которое ему прислали
 def parrot(bot, update):
@@ -20,7 +31,8 @@ def main():
 
     my_bot.dispatcher.add_handler(CommandHandler('start', sms)) #  dispather  принимает сообщение от Телеграмма входящее,
     #  addheandler передает в CommandHeandler подписанный на реагирование опредленных событий выполняет следующие действия (когда нажмут старт будет выхвана функция sms()
-
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Начать'), sms))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Анекдот'), get_anecdote)) # обрабатываем текст кнопки
     my_bot.dispatcher.add_handler(MessageHandler(Filters.text, parrot)) # обработчик текстового сообщения
 
     my_bot.start_polling() #проверяет о наличии сообщений с платформы Telegram
